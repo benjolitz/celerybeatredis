e957f3a58f0120e66b46c1e756e63a6f66e4f413
Ben Jolitz <ben.jolitz@xad.com>
2017-02-23 17:50:45 -0800

    silent linter freaking out

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup
 
 setup(
-    name = "celerybeat-redis",
-    description = "A Celery Beat Scheduler that uses Redis to store both schedule definitions and status information",
-    version = "0.1.6",
-    license = "Apache License, Version 2.0",
-    author = "Kong Luoxing",
-    author_email = "kong.luoxing@gmail.com",
-    home_page = 'https://github.com/kongluoxing/celerybeatredis',
-    maintainer = "Kong Luoxing",
-    maintainer_email = "kong.luoxing@gmail.com",
+    name="celerybeat-redis",
+    description="A Celery Beat Scheduler that uses Redis to store both schedule definitions and status information",
+    version="0.1.6",
+    license="Apache License, Version 2.0",
+    author="Kong Luoxing",
+    author_email="kong.luoxing@gmail.com",
+    home_page='https://github.com/kongluoxing/celerybeatredis',
+    maintainer="Kong Luoxing",
+    maintainer_email="kong.luoxing@gmail.com",
 
-    keywords = "python celery beat redis",
+    keywords="python celery beat redis",
 
-    packages = [
+    packages=[
         "celerybeatredis"
     ],
 
     install_requires=[
         'setuptools',
-        'redis>=2.10.3',
-        'celery>=3.1.16'
+        'redis>=2.10.3, <3.0.0a0',
+        'celery>=3.1.16, <4.0.0a0'
     ]
 
 )
